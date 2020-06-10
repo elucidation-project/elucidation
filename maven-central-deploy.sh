a7c260efb8b0bafa2ec1db76cec0ea932891fe32
Scott Leberknight <scottleber@gmail.com>
2020-06-09 21:54:17 -0400

    Fix SC2162 in maven-central-deploy.sh
    * Fixes "read without -r will mangle backslashes" problem
      described here: https://github.com/koalaman/shellcheck/wiki/SC2162

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -2,7 +2,7 @@
 # Deploy maven artifact in current directory into Maven central repository
 # using maven-release-plugin goals
 
-read -p "Really deploy to maven central repository  (yes/no)? "
+read -r -p "Really deploy to maven central repository  (yes/no)? "
 
 if [[ "$REPLY" == "yes" ]]; then
   mvn release:clean release:prepare release:perform -e | tee maven-central-deploy.log
