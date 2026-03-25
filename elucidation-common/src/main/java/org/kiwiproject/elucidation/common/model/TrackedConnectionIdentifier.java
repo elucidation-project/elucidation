5944d190d79ab8a6f68f195adb8c115c8079700e
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-03-24 21:48:55 -0400

    docs: misc grammar fixes; reflow javadoc

--- elucidation-common/src/main/java/org/kiwiproject/elucidation/common/model/TrackedConnectionIdentifier.java
+++ elucidation-common/src/main/java/org/kiwiproject/elucidation/common/model/TrackedConnectionIdentifier.java
@@ -7,13 +7,15 @@ import lombok.With;
 import org.kiwiproject.elucidation.common.definition.CommunicationDefinition;
 
 /**
- * A representation of a connection identifier that exists in the system. The purpose of this model is to be able to track
- * if a connection identifier is never used.
+ * A representation of a connection identifier that exists in the system.
+ * The purpose of this model is to be able to track if a connection identifier is never used.
  * <p>
  * <p>
- * Note that this makes more sense to use for communication types like HTTP. This is because for JMS types we would most likely
- * receive the Produce and the Consume events, so it makes it very easy to detect anything that is being produced but never consumed,
- * however, for HTTP, we would never know if an endpoint exists but is never called, because calling it would trigger the event.
+ * Note that it makes more sense to use this for communication types like HTTP.
+ * This is because for JMS types we would most likely receive the Produce and the Consume events.
+ * So, it makes it very easy to detect anything that is being produced but never consumed. However,
+ * for HTTP, we would never know if an endpoint exists but is never called, because calling it
+ * would trigger the event.
  */
 @Builder
 @Value
